12a87c1e3adc9efb3343ab1d777816d58819b72d
thobui <thohnb@gmail.com>
2025-01-20 20:12:56 +0700

    Placeholder

--- home.nix
+++ home.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "thobui"; # Placeholder for username
-  home.homeDirectory = "/Users/thobui"; # Placeholder for home directory
+  home.username = "__USERNAME__"; # Placeholder for username
+  home.homeDirectory = "__HOMEDIR__"; # Placeholder for home directory
   home.stateVersion = "24.11"; 
   home.packages = [
     pkgs.nerd-fonts.iosevka
@@ -18,44 +18,45 @@
     pkgs.fnm
     pkgs.zoxide
   ];
-programs.zsh = {
-# -- config terminal
-enable = true;
-initExtra = ''
-       if [[ $(ps -o command= -p "$PPID" | awk '{print $1}') != 'fish' ]]
+
+  programs.zsh = {
+    enable = true;
+    initExtra = ''
+      if [[ $(ps -o command= -p "$PPID" | awk '{print $1}') != 'fish' ]]
       then
           exec fish -l
-      fi                       '';
+      fi
+    '';
+  };
 
-};
-programs.fish = {
-	enable = true;
-	#-- plugins of fish
-	interactiveShellInit = ''
-	# Disable greetings line
-	set -g fish_greeting
-	# Add fnm shell-fish env
-	fnm env --use-on-cd --shell fish | source
-	# Add zoxide shell-fish env
-        zoxide init fish | source
-	'';
-};
-# -- Zoxide
-programs.zoxide={
-enable = true;
-enableFishIntegration= true;
-};
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      # Disable greetings line
+      set -g fish_greeting
+      # Add fnm shell-fish env
+      fnm env --use-on-cd --shell fish | source
+      # Add zoxide shell-fish env
+      zoxide init fish | source
+    '';
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
   # Symlink configuration for Alacritty
   xdg.configFile = {
     "alacritty" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/alacritty";
+      source = config.lib.file.mkOutOfStoreSymlink "__HOMEDIR__/config/alacritty";
       recursive = true;
     };
   };
 
   # Program configuration
   programs.gh.enable = true;
-  
+
   # Enable home-manager to manage itself
   programs.home-manager.enable = true;
 }
