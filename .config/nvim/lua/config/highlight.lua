3c9b54e551570506d4d7a99849445ef889c6dd65
Bùi Nguyễn Hoàng Thọ <thohnb@gmail.com>
2025-02-20 22:44:29 +0700

    Change ghosttext color

--- .config/nvim/lua/config/highlight.lua
+++ .config/nvim/lua/config/highlight.lua
@@ -4,3 +4,4 @@ highlight(0, "MiniPickPrompt", {bg = "NONE"})
 highlight(0, "MiniPickMatchCurrent", {bg = "NONE", fg = "#ffffff"})
 -- Built-in
 highlight(0, "CursorLine", {bg = "NONE", fg = "#ffffff"})
+highlight(0, "BlinkCmpGhostText", {bg = "NONE", fg = "#5F6776"})
